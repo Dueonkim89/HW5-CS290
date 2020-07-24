44fc7870bc01ae64efc76fee4cfeec82498ab9aa
unknown <dueonkim89@yahoo.com>
2020-07-23 17:55:22 -0700

    templates & route handlers created

--- app.js
+++ app.js
@@ -6,28 +6,49 @@ var handlebars = require("express-handlebars").create({
 });
 var bodyParser = require("body-parser");
 
+//parse JSON data and url encoded data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// set up handlebars middleware
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
-app.set("port", 64777);
+// listen on port 64778
+app.set("port", 64778);
 
+// render the GetRequest template for / GET requests
 app.get("/", function(req, res) {
-  res.type("text/plain");
-  res.send("Welcome to the main page!");
+	// get keys of data objet
+	let dataKeys = Object.keys(req.query);
+	// map the keys
+	let formattedData = dataKeys.map(key => ({'data': key, 'value': req.query[key]}));
+	// object with info
+	let dataToDisplay = {'clientInfo': formattedData};
+	// render the template and send data
+	res.render('GetRequest', dataToDisplay);
 });
 
-app.get("/other-page", function(req, res) {
-  res.type("text/plain");
-  res.send("Welcome to the other page!");
+// for POST requests, render PostRequest template
+app.post('/', function(req,res){
+	// get keys of query object
+	let dataKeys = Object.keys(req.query);
+	// map the query keys
+	let formattedData = dataKeys.map(key => ({'data': key, 'value': req.query[key]}));
+	// console.log(req.body)
+	// get keys from body of request
+	let bodyKeys = Object.keys(req.body);
+	// map the data in req.body
+	let formattedBodyData = bodyKeys.map(key => ({'data': key, 'value': req.body[key]}));
+	// object with info
+	let dataToDisplay = {'urlInfo': formattedData, 'bodyInfo': formattedBodyData};
+	// render the template and send data
+	res.render('PostRequest', dataToDisplay);
 });
 
 app.use(function(req, res) {
-  res.type("text/plain");
   res.status(404);
-  res.send("404 - Not Found");
+  res.render("404");
 });
 
 app.use(function(err, req, res, next) {
